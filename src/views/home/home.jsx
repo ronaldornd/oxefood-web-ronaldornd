eff0857ce27f64c0b62b805a715d285a239c99cb
Ronaldo Barbosa <60077368+ronaldornd@users.noreply.github.com>
2023-09-01 17:02:27 -0300

    Update home.jsx links

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -17,8 +17,8 @@ export default function Home () {
                               
                                Bem vindo ao sistema <strong>OxeFood</strong> ! <br/>
                                Este sistema foi desenvolvido na disciplina de Desenvolvimento para WEB III. <br/> <br/>
-                               Para acessar o código da <strong>API</strong> do sistema, acesse: <a href='https://github.com/robertoalencar/oxefood-api' target='_blank'> https://github.com/robertoalencar/oxefood-api </a> <br/> <br/>
-                               Para acessar o código do <strong>Módulo WEB</strong>, acesse: <a href='https://github.com/robertoalencar/oxefood-web' target='_blank'> https://github.com/robertoalencar/oxefood-web </a>
+                               Para acessar o código da <strong>API</strong> do sistema, acesse: <a href='https://github.com/ronaldornd/oxefood-api-ronaldornd' target='_blank'> https://github.com/ronaldornd/oxefood-api-ronaldornd </a> <br/> <br/>
+                               Para acessar o código do <strong>Módulo WEB</strong>, acesse: <a href='https://github.com/ronaldornd/oxefood-web-ronaldornd' target='_blank'> https://github.com/ronaldornd/oxefood-web-ronaldornd </a>
 
                            </Grid.Column>
                        </Grid.Row>
