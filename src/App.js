633c76dc447bff21364eb1e6a6e97f569b7ad0e1
Ronaldo Barbosa <60077368+ronaldornd@users.noreply.github.com>
2023-09-08 17:31:19 -0300

    pag de cadastro concluida

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { Segment } from 'semantic-ui-react';
 import './App.css';
 import Home from './views/home/home.jsx';
 import FormCliente from './views/cliente/FormCliente';
+import FormProduto from './views/produto/FormProduto';
 function App() {
   return (
     <div className="App">
-      <FormCliente/>
+      <FormProduto/>
        <div style={{marginTop: '6%'}}>
         <Segment vertical color='grey' size='tiny' textAlign='center'>
           &copy; 2023 - Projeto WEB III - IFPE Jaboatão dos Guararapes
